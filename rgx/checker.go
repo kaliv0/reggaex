e912d276e9242fd63f0550fe8c507245ff35a53b
kaliv0 <kaloyan.ivanov88@gmail.com>
2024-08-25 13:51:23 +0300

    added negative escape sequences, whitespaces and refactored tests

--- rgx/checker.go
+++ rgx/checker.go
@@ -63,7 +63,6 @@ func isQuestion(b byte) bool {
 
 func isLiteral(b byte) bool {
 	return unicode.IsLetter(rune(b)) || unicode.IsDigit(rune(b)) || isNonWordSymbol(b)
-	//return (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || (b >= '0' && b <= '9') || (b == ' ' || b == ':' || b == '/')
 }
 
 func isNonWordSymbol(b byte) bool {
@@ -79,29 +78,40 @@ func isUnit(term string) bool {
 }
 
 func doesUnitMatch(expr string, str string) bool {
-	head, _, _ := splitExpr(expr)
-
 	if len(str) == 0 {
 		return false
 	}
 
-	// TODO: head is only one char?
+	head, _, _ := splitExpr(expr)
+
 	if isLiteral(head[0]) {
 		return expr[0] == str[0]
 	} else if isDot(head[0]) {
 		return true
 	} else if isEscapeSequence(head) {
-		// TODO: fix \\a
-		if head == "\\a" {
-			return unicode.IsLetter(rune(str[0]))
-		} else if head == "\\d" {
-			return unicode.IsDigit(rune(str[0]))
-		} else {
-			return false
-		}
+		return evaluateEscapeSequence(head, str)
 	} else if isSet(head) {
 		setTerms := splitSet(head)
 		return strings.ContainsRune(setTerms, rune(str[0]))
 	}
 	return false
+}
+
+// TODO: rename func
+func evaluateEscapeSequence(head string, str string) bool {
+	if head == "\\w" {
+		return unicode.IsLetter(rune(str[0])) || unicode.IsDigit(rune(str[0])) || str[0] == '_'
+	} else if head == "\\d" {
+		return unicode.IsDigit(rune(str[0]))
+	} else if head == "\\s" {
+		return unicode.IsSpace(rune(str[0]))
+	} else if head == "\\W" {
+		return !unicode.IsLetter(rune(str[0])) && !unicode.IsDigit(rune(str[0])) && str[0] != '_'
+	} else if head == "\\D" {
+		return !unicode.IsDigit(rune(str[0]))
+	} else if head == "\\S" {
+		return !unicode.IsSpace(rune(str[0]))
+	} else {
+		return false
+	}
 }
--- tests/rgx_test.go
+++ tests/rgx_test.go
@@ -2,13 +2,13 @@ package tests
 
 import (
 	"github.com/stretchr/testify/assert"
-	"reggaex/rgx"
 	"testing"
+
+	"reggaex/rgx"
 )
 
-// with string limits
-func Test1(t *testing.T) {
-	expr := `^http://(\a|\d)+.(com|net|org)[@/#]+.*$`
+func TestStrLimits(t *testing.T) {
+	expr := `^http://\w+.(com|net|org)[@/#]+.*$`
 	str := `http://qwerty123.com@hey/there`
 	matched, matchPos, matchLen := rgx.Match(expr, str)
 	expected := str[matchPos : matchPos+matchLen]
@@ -16,25 +16,22 @@ func Test1(t *testing.T) {
 	assert.Equal(t, expected, str)
 }
 
-// with string limits fails at start
-func Test2(t *testing.T) {
-	expr := `^http://(\a|\d)+.(com|net|org)[@/#]+.*$`
+func TestStrLimitsFailsAtStart(t *testing.T) {
+	expr := `^http://w+.(com|net|org)[@/#]+.*$`
 	str := `xxxhttp://qwerty123.com@hey/there`
 	matched, _, _ := rgx.Match(expr, str)
 	assert.False(t, matched)
 }
 
-// with string limits fails at end
-func Test3(t *testing.T) {
-	expr := `^http://(\a|\d)+.(com|net|org)[@/#]+[ab]$`
+func TestStrLimitsFailsAtEnd(t *testing.T) {
+	expr := `^http://\w+.(com|net|org)[@/#]+[ab]$`
 	str := `http://qwerty123.com@xxx`
 	matched, _, _ := rgx.Match(expr, str)
 	assert.False(t, matched)
 }
 
-// no start limit
-func Test4(t *testing.T) {
-	expr := `http://(\a|\d)+.(com|net|org)[@/#]+.*$`
+func TestNoStartLimit(t *testing.T) {
+	expr := `http://\w+.(com|net|org)[@/#]+.*$`
 	str := `xxxhttp://qwerty123.com@hey/there`
 	matched, matchPos, matchLen := rgx.Match(expr, str)
 	actual := str[matchPos : matchPos+matchLen]
@@ -43,13 +40,127 @@ func Test4(t *testing.T) {
 	assert.Equal(t, actual, expected)
 }
 
-// no end limit
-func Test5(t *testing.T) {
-	expr := `^http://(\a|\d)+.(com|net|org)[@/#]+`
+func TestNoEndLimit(t *testing.T) {
+	expr := `^http://\w+.(com|net|org)[@/#]+`
 	str := `http://qwerty123.com@xxx`
 	matched, matchPos, matchLen := rgx.Match(expr, str)
 	actual := str[matchPos : matchPos+matchLen]
 	expected := `http://qwerty123.com@`
 	assert.True(t, matched)
 	assert.Equal(t, actual, expected)
+}
+
+// escape sequences
+func TestWhitespace(t *testing.T) {
+	expr := `^\s[abc]+\s[xyz]+$`
+	str := ` abc	yzx`
+	matched, matchPos, matchLen := rgx.Match(expr, str)
+	expected := str[matchPos : matchPos+matchLen]
+	assert.True(t, matched)
+	assert.Equal(t, expected, str)
+}
+
+func TestWhitespaceFails(t *testing.T) {
+	expr := `^\s[abc]+\s[xyz]+$`
+	str := `abc	yzx`
+	matched, _, _ := rgx.Match(expr, str)
+	assert.False(t, matched)
+}
+
+func TestDigit(t *testing.T) {
+	expr := `^\d+$`
+	str := `123`
+	matched, matchPos, matchLen := rgx.Match(expr, str)
+	expected := str[matchPos : matchPos+matchLen]
+	assert.True(t, matched)
+	assert.Equal(t, expected, str)
+}
+
+func TestDigitFails(t *testing.T) {
+	expr := `^\d+$`
+	str := `1xx`
+	matched, _, _ := rgx.Match(expr, str)
+	assert.False(t, matched)
+}
+
+func TestWordChar(t *testing.T) {
+	expr := `^\w+$`
+	str := `xyz_a23`
+	matched, matchPos, matchLen := rgx.Match(expr, str)
+	expected := str[matchPos : matchPos+matchLen]
+	assert.True(t, matched)
+	assert.Equal(t, expected, str)
+}
+
+func TestWordCharFails(t *testing.T) {
+	expr := `^\w+$`
+	str := `x z`
+	matched, _, _ := rgx.Match(expr, str)
+	assert.False(t, matched)
+}
+
+// negative escape sequences
+func TestNegWhitespace(t *testing.T) {
+	expr := `^\S+$`
+	str := `xzy123./?`
+	matched, matchPos, matchLen := rgx.Match(expr, str)
+	expected := str[matchPos : matchPos+matchLen]
+	assert.True(t, matched)
+	assert.Equal(t, expected, str)
+}
+
+func TestNegWhitespaceFails(t *testing.T) {
+	expr := `^\S+$`
+	str := `xzy 123./?`
+	matched, _, _ := rgx.Match(expr, str)
+	assert.False(t, matched)
+}
+
+func TestNegDigit(t *testing.T) {
+	expr := `^\D+$`
+	str := `xzy ./?`
+	matched, matchPos, matchLen := rgx.Match(expr, str)
+	expected := str[matchPos : matchPos+matchLen]
+	assert.True(t, matched)
+	assert.Equal(t, expected, str)
+}
+
+func TestNegDigitFails(t *testing.T) {
+	expr := `^\D+$`
+	str := `xzy 22./?`
+	matched, _, _ := rgx.Match(expr, str)
+	assert.False(t, matched)
+}
+
+func TestNegWordChar(t *testing.T) {
+	expr := `^\W+$`
+	str := `: ./?`
+	matched, matchPos, matchLen := rgx.Match(expr, str)
+	expected := str[matchPos : matchPos+matchLen]
+	assert.True(t, matched)
+	assert.Equal(t, expected, str)
+}
+
+func TestNegWordCharFails(t *testing.T) {
+	expr := `^\W+$`
+	str := `x_1./?`
+	matched, _, _ := rgx.Match(expr, str)
+	assert.False(t, matched)
+}
+
+// sets
+func TestSet(t *testing.T) {
+	expr := `^[abc]+$`
+	str := `aabcbba`
+	matched, matchPos, matchLen := rgx.Match(expr, str)
+	expected := str[matchPos : matchPos+matchLen]
+	assert.True(t, matched)
+	assert.Equal(t, expected, str)
+}
+
+func TestSetFails(t *testing.T) {
+	expr := `^[abc]+$`
+	str := `ab123a`
+	matched, _, _ := rgx.Match(expr, str)
+	assert.False(t, matched)
 }
