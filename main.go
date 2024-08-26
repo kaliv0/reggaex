6fcd673629824c4f162a5501dc6ed1196ba96bcc
Kaloyan Ivanov <kaloyan.ivanov88@gmail.com>
2024-08-26 09:53:01 +0300

    Create go.yml

--- main.go
+++ main.go
@@ -1,36 +1,15 @@
 package main
 
-import (
-	"fmt"
-
-	// "github.com/kaliv0/reggaex/rgx/matcher"
-	"reggaex/rgx"
-)
-
-// type Matcher struct {
-// 	pattern    string
-// 	expression string
-
-// 	subPattern    string
-// 	subExpression string
-// }
-
-// func (m *Matcher) setSubPattern(start, end int){
-// 	m.subPattern = m.pattern[start:end]
-// }
-
 func main() {
-	expr := `^http://(\a|\d)+.(com|net|org)[@/#]+.*$`
-	str := `http://qwerty123.com@hey/there`
-	matcher := rgx.Matcher{Expr: expr}
-	//str := `http://clumsy_123_computer.com@hey/there`
-	//str := ""
+	//expr := `^http://(\a|\d)+.(com|net|org)[@/#]+.*$`
+	//str := `http://qwerty123.com@hey/there`
+	//matcher := rgx.Matcher{Expr: expr}
 
-	matched, matchPos, matchLen := matcher.Match(expr, str)
-	if matched {
-		matchRange := str[matchPos : matchPos+matchLen]
-		fmt.Printf("matchExpr(`%s`, '%s') = %s", expr, str, matchRange)
-	} else {
-		fmt.Printf("matchExpr(`%s`, '%s') = False", expr, str)
-	}
+	//matched, matchPos, matchLen := matcher.Match(expr, str)
+	//if matched {
+	//	matchRange := str[matchPos : matchPos+matchLen]
+	//	fmt.Printf("matchExpr(`%s`, '%s') = %s", expr, str, matchRange)
+	//} else {
+	//	fmt.Printf("matchExpr(`%s`, '%s') = False", expr, str)
+	//}
 }
